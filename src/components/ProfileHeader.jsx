1d21890de19f805f61e039193ab422bf37f41985
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-05-25 00:16:44 +0530

    Profile Header in Sidebar

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,5 +1,107 @@
+import { useRef, useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
+import { LogOutIcon, Volume2Icon, VolumeOffIcon } from "lucide-react";
+
+const mouseClickSound = new Audio("/sounds/mouse-click.mp3");
+
 function ProfileHeader() {
-  return <div>ProfileHeader</div>;
+  const { logout, authUser, updateProfile } = useAuthStore();
+  const { isSoundEnabled, toggleSound } = useChatStore();
+
+  const [selectedImg, setSelectedImg] = useState(null);
+
+  const fileInputRef = useRef(null);
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = async () => {
+      const base64Img = reader.result;
+      setSelectedImg(base64Img);
+      await updateProfile({ profilePic: base64Img });
+    };
+  };
+
+  return (
+    <div className="p-6 border-b border-slate-700/50">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          {/* AVATAR */}
+          <div className="avatar online">
+            <button
+              onClick={() => fileInputRef.current.click()}
+              className="size-14 rounded-full overflow-hidden relative group cursor-pointer"
+            >
+              <img
+                src={selectedImg || authUser.profilrPic || "/avatar.png"}
+                alt="User Img"
+                className="size-full object-cover"
+              />
+
+              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
+                <span className="text-white text-xs">Change</span>
+              </div>
+            </button>
+
+            <input
+              type="file"
+              name="avatarImg"
+              id="avatarImg"
+              accept="image/*"
+              ref={fileInputRef}
+              onChange={handleImageUpload}
+              className="hidden"
+            />
+          </div>
+
+          {/* USERNAME & ONLINE TEXT */}
+          <div>
+            <h3 className="text-slate-200 font-medium text-base max-w-[180px] truncate">
+              {authUser.fullName}
+            </h3>
+
+            <p className="text-slate-400 text-xs">Online</p>
+          </div>
+        </div>
+
+        {/* BUTTONS */}
+        <div className="flex gap-4 items-center">
+          {/* LOGOUT BTN */}
+          <button
+            className="text-slate-400 hover:text-slate-200 transition-colors"
+            onClick={logout}
+          >
+            <LogOutIcon className="size-5" />
+          </button>
+
+          {/* SOUND TOGGLE BTN */}
+          <button
+            className="text-slate-400 hover:text-slate-200 transition-colors"
+            onClick={() => {
+              // play sound before toggling
+              mouseClickSound.currentTime = 0; // reset to start
+              mouseClickSound
+                .play()
+                .catch((error) => console.log("Audio play failed: ", error));
+              toggleSound();
+            }}
+          >
+            {isSoundEnabled ? (
+              <Volume2Icon className="size-5" />
+            ) : (
+              <VolumeOffIcon className="size-5" />
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileHeader;
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -66,4 +66,16 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }
   },
+
+  updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully!");
+    } catch (error) {
+      console.log("Error in update profile: ", error);
+      toast.error(error.response.data.message);
+    }
+  },
 }));
